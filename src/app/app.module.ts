87e2967fcaeae25248df5c175b7acbed37bc9e81
Cezary <cezary.janicki@gmail.com>
2021-12-21 22:24:54 +0100

    Division to components, final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { FormComponent } from './form/form.component';
 import { ButtonComponent } from './button/button.component';
+import { VoteButtonsComponent } from './vote-buttons/vote-buttons.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, RankingComponent, FormComponent, ButtonComponent],
+  declarations: [AppComponent, NavbarComponent, RankingComponent, FormComponent, ButtonComponent, VoteButtonsComponent, ListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
