6cb38b68859626054111d82c3dbb6c0cd8cf3551
Cezary <cezary.janicki@gmail.com>
2021-12-21 10:19:03 +0100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {Quotation} from './models/quotation';
+import {QUOTES} from './models/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author: '', sentence: '', votes: 0}
+  }
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
+}
